2c0e438c88e19175d1247adccd38f474bc457892
John Ottenlips <johno@aloompa.com>
2019-11-29 10:42:14 -0600

    first unit test is passing

--- features/Songs/tests/test_song.py
+++ features/Songs/tests/test_song.py
@@ -1,10 +1,12 @@
 
 from features.Songs.song import resolve_song
 from aws_resources.mock_dynamo import setup_mocks, mock_song
+from moto import mock_dynamodb2
 
+@mock_dynamodb2
 def test_resolve_song():
     setup_mocks()
     song = resolve_song({}, {}, "1")
     print(mock_song)
     print(song, '::::song')
-    assert 1 is 1
+    assert song == mock_song
