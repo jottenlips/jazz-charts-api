bb11e085ce2260bf75a3e56d90b98c9f39c13db7
John Ottenlips <johno@aloompa.com>
2019-11-29 19:21:06 -0600

    remove commented code

--- aws_resources/tests/dynamo_test.py
+++ aws_resources/tests/dynamo_test.py
@@ -1,13 +1,6 @@
 from aws_resources.mock_dynamo import mock_song
 from aws_resources.dynamo import build_update_expression, build_update_attributes_dictionary
 
-# mock_song = {
-#             'id': '1',
-#             "title": "It don't mean a thing",
-#             "chordChart": "Gm | ...",
-#             "composer": "c1"
-#         }
-
 def test_build_update_expression():
 
     update_exp = build_update_expression(mock_song)
