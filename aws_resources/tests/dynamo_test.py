467a694e923193e476f3146fdf185c6153496b25
John Ottenlips <johno@aloompa.com>
2019-11-29 19:20:50 -0600

    add test for dynamo helpers

---
+++ aws_resources/tests/dynamo_test.py
@@ -0,0 +1,24 @@
+from aws_resources.mock_dynamo import mock_song
+from aws_resources.dynamo import build_update_expression, build_update_attributes_dictionary
+
+# mock_song = {
+#             'id': '1',
+#             "title": "It don't mean a thing",
+#             "chordChart": "Gm | ...",
+#             "composer": "c1"
+#         }
+
+def test_build_update_expression():
+
+    update_exp = build_update_expression(mock_song)
+    print(update_exp)
+    assert update_exp == 'set title=:title, chordChart=:chordChart, composer=:composer'
+
+def build_update_attributes_dictionary():
+    update_attr = pdate_attributes_dictionary(mock_song)
+    assert update_attr == {
+        ':title': 'It don\'t mean a thing',
+        ':chordChart': 'Gm | ...',
+        ':composer': 'c1'
+    }
+
